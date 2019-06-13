5132c0b4515a669b38d976eb6458b999c0277eb5
Travis Exline <t.exline22@gmail.com>
2019-06-13 14:46:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenre.name FROM authors
   INNER JOIN series ON series.author_id
-  INNER JOIN subgenre ON series.subgenre_id;"
+  INNER JOIN subgenres ON series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
