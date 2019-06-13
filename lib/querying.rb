4b4ad6a12e6da75c84203cd09545b3e0ef4810ef
Travis Exline <t.exline22@gmail.com>
2019-06-13 14:55:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   INNER JOIN characters ON series.id == characters.series_id
-  WHERE character.species = 'human' DESC 1;"
+  WHERE character.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
