3042da882a1126755d602f5c96bd6f612d19e68d
Travis Exline <t.exline22@gmail.com>
2019-06-13 15:01:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name COUNT(*) as book_list FROM characters
+  "SELECT characters.name, COUNT(*) as book_list FROM characters
   INNER JOIN character_books ON books.id == character_books.book_id
   GROUP BY characters.name
   ORDER BY book_list DESC;"
